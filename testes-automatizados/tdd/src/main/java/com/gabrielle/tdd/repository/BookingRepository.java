d3ca9daf8d6c891466d9117776a8e244f089ae3c
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2022-10-21 12:21:50 -0300

    Created Controller Test

--- testes-automatizados/tdd/src/main/java/com/gabrielle/tdd/repository/BookingRepository.java
+++ testes-automatizados/tdd/src/main/java/com/gabrielle/tdd/repository/BookingRepository.java
@@ -3,6 +3,9 @@ package com.gabrielle.tdd.repository;
 import com.gabrielle.tdd.model.BookingModel;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface BookingRepository extends JpaRepository<BookingModel, String> {
 
     Optional<BookingModel> findByReserveName(String reserveName);
